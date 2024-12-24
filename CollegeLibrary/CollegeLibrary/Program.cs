0090f174010cfb601ee015aab0e1de8a415fe96a
Елена Родишевская <156431934+Lena20051@users.noreply.github.com>
2024-12-24 10:48:02 +0300

    Add files via upload

---
+++ CollegeLibrary/CollegeLibrary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
